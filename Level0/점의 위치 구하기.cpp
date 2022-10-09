5d6d6e85dc0446646c40b16156c54bf96cf10f18
Hades000 <lsw463@naver.com>
2022-10-10 05:53:10 +0900

    Solve: 점의 위치 구하기

---
+++ Level0/점의 위치 구하기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    int answer = 0;
+
+    if(dot[0] > 0 && dot[1] > 0)
+        answer = 1;
+    else if(dot[0] < 0 && dot[1] >0)
+        answer = 2;
+    else if(dot[0] < 0 && dot[1] < 0)
+        answer = 3;
+    else 
+        answer = 4;
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> ex;
+    ex.push_back(-7);
+    ex.push_back(9);
+    cout << solution(ex) << endl;
+
+    return 0;
+}
